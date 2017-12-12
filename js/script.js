535aaf8788398afbc3f97bb7a5163d0108e311b2
rubinj30 <jonathan.a.rubin@gmail.com>
2017-12-12 12:52:50 -0500

    Starting deal first four cards and to track dealer and player values. "Hit" button is working.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-// should i put these into an object? 
+// Create Deck
 const suits = ['H', 'D', 'S', 'C']
 const cards = 'A23456789TJQK'
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 
 function createDeckOfCards() {
     const deckOfCards = []
@@ -41,5 +40,74 @@ function shuffleCards(deckToShuffle) {
     return newDeck;
   }
 
-// create new array of shuffled cards
+// Create new array with shuffled cards
+
 const shuffledDeckOfCards = shuffleCards(unshuffledDeckOfCards)
+
+
+
+
+
+let playerCardsValue = 0
+let dealerCardsValue = 0
+let playerCardsArray = []
+let dealerCardsArray = []
+let playerCardsImages = []
+
+
+function dealFirstFourCards() {
+    for (let i = 0; i < 4; i++) {
+        const dealtCardObj = shuffledDeckOfCards.shift()
+        if (i % 2 === 0) {
+            playerCardsValue += dealtCardObj.value
+            playerCardsArray.push(dealtCardObj)
+            playerCardsImages.push(`${dealtCardObj.value}${dealtCardObj.suit}.png`)
+        }
+        else {
+            dealerCardsValue += dealtCardObj.value
+            dealerCardsArray.push(dealtCardObj)
+            dealerCardsImages.push(`${dealtCardObj.value}${dealtCardObj.suit}.png`)
+        }
+    console.log(`player: ${playerCardsValue} dealer: ${dealerCardsValue}`)
+    return playerCardsValue, dealerCardsValue
+    }
+}
+
+dealFirstFourCards()
+
+
+// deals card and outputs dealtCard object, image, and value
+let dealtCardObj = {}
+let dealtCardImage = ''
+let dealtCardValue = 0
+
+
+$('#hit-button').on('click', function() {
+    dealtCardObj = shuffledDeckOfCards.shift()
+    dealtCardImage = `${dealtCardObj.value}${dealtCardObj.suit}.png`
+    dealtCardValue = dealtCardObj.value
+    console.log(dealtCardValue)
+    console.log(dealtCardImage)
+    playerCardsValue += dealtCardValue
+    return dealtCardObj, dealtCardImage, dealtCardValue
+})
+
+
+
+
+$('#player-cards').innerHTML = dealtCardImage
+
+// logic for determining winner
+// requires totals as parameters, which will only be provided once both players are done getting cards
+function compareDealerAndPlayerTotals(dealerTotal, playerTotal) {
+    
+    if (dealerTotal > playerTotal) {
+        return 'The dealer wins this round.'
+    }
+    else if (dealerTotal < playerTotal) {
+        return 'You win the round!'
+    }
+    else {
+        return 'This round is a draw.'
+    }
+}
